ea27e59ea02e02bedf7483ca3514e2a934c02987
Samuel Tijani <omalitijani@gmail.com>
2020-07-11 18:52:16 +0100

    Create lifestyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { AccomodationComponent } from './accomodation/accomodation.component';
 import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccomodationComponent,
-    FoodBeverageComponent
+    FoodBeverageComponent,
+    LifestyleComponent
   ],
   imports: [
     BrowserModule,
