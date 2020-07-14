47efb95e70fd4d5e48bc1f3403d995fb5a1cf265
Samuel Tijani <omalitijani@gmail.com>
2020-07-14 15:29:09 +0100

    Rename component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { AccomodationComponent } from './accomodation/accomodation.component';
-import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
 import { LifestyleComponent } from './lifestyle/lifestyle.component';
 import { WeddingsComponent } from './weddings/weddings.component';
 import { CelebrationsComponent } from './celebrations/celebrations.component';
 import { OffersComponent } from './offers/offers.component';
 import { AuthComponent } from './auth/auth.component';
 import { RoomsComponent } from './accomodation/rooms/rooms.component';
-import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
-import { BarsComponent } from './food-beverage/bars/bars.component';
+import { RestaurantComponent } from './food-and-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-and-beverage/bars/bars.component';
 import { WellnessAndSpaComponent } from './lifestyle/wellness-and-spa/wellness-and-spa.component';
 import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
 import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
@@ -28,7 +28,7 @@ import { SignupComponent } from './auth/signup/signup.component';
   declarations: [
     AppComponent,
     AccomodationComponent,
-    FoodBeverageComponent,
+    FoodAndBeverageComponent,
     LifestyleComponent,
     WeddingsComponent,
     CelebrationsComponent,
