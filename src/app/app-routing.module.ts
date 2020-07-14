7c9754d0e7051e32697188d5f4287a7169dd6bb3
Samuel Tijani <omalitijani@gmail.com>
2020-07-14 16:05:11 +0100

    Define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { OffersComponent } from './offers/offers.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accomodation', component: AccomodationComponent},
+  {path: 'celebrations', component: CelebrationsComponent},
+  {path: 'food-and-beverage', component: FoodAndBeverageComponent},
+  {path: 'lifestyle', component: LifestyleComponent},
+  {path: 'weddings', component: WeddingsComponent},
+  {path: 'offers', component: OffersComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'signin', component: SigninComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
